4756158bab6744da9860f15932c975561063dcc8
RuanHub <ruanvanvuuren9@gmail.com>
2024-09-26 15:30:59 +0400

    Autoclose neo-tree on file open

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -21,5 +21,14 @@ return {
         },
       },
     },
+    event_handlers = {
+      {
+        event = 'file_opened',
+        handler = function()
+          --auto close
+          require('neo-tree').close_all()
+        end,
+      },
+    },
   },
 }
